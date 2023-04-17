d1be1a8e60703f4d3ca2681724071f67413de009
dominids <dominiklyszynski@gmail.com>
2023-04-17 14:48:36 +0200

    główne

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice', class:'support' },
+    { id: 13, name: 'Bombasto', class: 'attack' },
+    { id: 14, name: 'Celeritas', class: 'flanker' },
+    { id: 15, name: 'Magneta', class: 'support' },
+    { id: 16, name: 'RubberMan', class: 'tank' },
+    { id: 17, name: 'Dynama', class: 'flanker' },
+    { id: 18, name: 'Dr. IQ', class: 'attack' },
+    { id: 19, name: 'Magma', class: 'tank' },
+    { id: 20, name: 'Tornado', class: 'attack' }
+];
