436e18bda2ca701f8e062eeee50f055749383dab
dominids <dominiklyszynski@gmail.com>
2023-04-20 21:16:34 +0200

    jebane indeksy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,18 @@ import {
   animations: [
     trigger('openClose', [
       state('open', style({
-        zIndex: '10'
+        zIndex:'10'
       })),
 
       state('closed', style({
         transform: 'translateY(-150%)',
+        zIndex:'-10'
       })),
 
       transition('open => closed', [
         animate('500ms ease',
           keyframes([
-            style({ zIndex: '-10',transform: 'translateY(-10%)' }),
+            style({ zIndex:'-10',transform: 'translateY(-10%)' }),
             style({ transform: 'translateY(-50%)' }),
             style({ transform: 'translateY(-80%)' }),
             style({ transform: 'translateY(-113%)',})
@@ -35,11 +36,11 @@ import {
       transition('closed => open', [
         animate('500ms ease',
           keyframes([
-            style({ transform: 'translateY(-95%)' }),
+            style({ zIndex:'-10',transform: 'translateY(-95%)' }),
             style({ transform: 'translateY(-80%)' }),
             style({ transform: 'translateY(-60%)' }),
             style({ transform: 'translateY(-30%)' }),
-            style({ transform: 'translateY(0)' })
+            style({ transform: 'translateY(0)'})
           ]))
       ]),
     ])
